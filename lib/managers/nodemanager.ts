d24a4c409f8f6d4db2814fddda2680c6b351c647
Iru21 <33987046+Iru21@users.noreply.github.com>
2021-08-04 11:53:43 +0200

    force pagination

--- lib/managers/nodemanager.ts
+++ lib/managers/nodemanager.ts
@@ -9,25 +9,21 @@ export default class NodeManager {
         this.client = _client;
     }
 
-    async list(handlePagination: boolean = false): Promise<Node[]> {
+    async list(): Promise<Node[]> {
         let current_page = (await this.client.call("nodes"))
         let returner = []
-        if(handlePagination) {
-            const pages = current_page.meta.pagination.total_pages
-            let attIterator = 0
-            for (let i = 0; i < pages; i++) {
-                for(let j = 0; j < current_page.data.length; j++) {
-                    returner[attIterator] = new Node(current_page.data[j].attributes)
-                    attIterator++
-                }
-                let next_link = current_page.meta.pagination.links.next
-                if(next_link) {
-                    next_link = next_link.replace(this.client.url, "")
-                    current_page = (await this.client.call("nodes"))
-                }
+        const pages = current_page.meta.pagination.total_pages
+        let attIterator = 0
+        for (let i = 0; i < pages; i++) {
+            for(let j = 0; j < current_page.data.length; j++) {
+                returner[attIterator] = new Node(current_page.data[j].attributes)
+                attIterator++
             }
-        } else {
-            returner = current_page.data
+            let next_link = current_page.meta.pagination.links.next
+            if(next_link) {
+                next_link = next_link.replace(this.client.url, "")
+                current_page = (await this.client.call("nodes"))
+            }
         }
         return returner
     }

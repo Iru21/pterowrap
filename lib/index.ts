92199785af0e08ed2e6936306947e77d200ffca9
Iru21 <33987046+Iru21@users.noreply.github.com>
2021-08-04 11:53:25 +0200

    finish location manager

--- lib/index.ts
+++ lib/index.ts
@@ -7,7 +7,7 @@ test()
 async function test() {
     const client = new AdminInstance("https://panel.discordbothosting.com/", "dFzy1VyYo8aE6lplE3mZ5exe9I9xtU1iDKdqg0Phkjhb9oVl");
     //console.log(await client.nodes.list(true))
-    console.log(await client.locations.list(true))
+    console.log(await client.locations.get(7))
 }
 
 export default {
--- lib/managers/locationmanager.ts
+++ lib/managers/locationmanager.ts
@@ -9,26 +9,65 @@ export default class LocationManager {
         this.client = _client;
     }
 
-    async list(handlePagination: boolean = false): Promise<Location[]> {
+    async list(): Promise<Location[]> {
         let current_page = (await this.client.call("locations"))
         let returner = []
-        if(handlePagination) {
-            const pages = current_page.meta.pagination.total_pages
-            let attIterator = 0
-            for (let i = 0; i < pages; i++) {
-                for(let j = 0; j < current_page.data.length; j++) {
-                    returner[attIterator] = new Location(current_page.data[j].attributes)
-                    attIterator++
-                }
-                let next_link = current_page.meta.pagination.links.next
-                if(next_link) {
-                    next_link = next_link.replace(this.client.url, "")
-                    current_page = (await this.client.call("locations"))
-                }
+        const pages = current_page.meta.pagination.total_pages
+        let attIterator = 0
+        for (let i = 0; i < pages; i++) {
+            for(let j = 0; j < current_page.data.length; j++) {
+                returner[attIterator] = new Location(current_page.data[j].attributes)
+                attIterator++
             }
-        } else {
-            returner = current_page.data
+            let next_link = current_page.meta.pagination.links.next
+            if(next_link) {
+                next_link = next_link.replace(this.client.url, "")
+                current_page = (await this.client.call("locations"))
+            }
         }
         return returner
+    }
+
+    async get(id: Number): Promise<Location> {
+        return new Location((await this.client.call("locations/" + id)).attributes)
+    }
+
+    async create(shortName: string, longName: string | null): Promise<Location> {
+        if(longName == null) longName = shortName
+        if(shortName.length > 60 || longName.length > 255) throw new Error("Provided shortName(amx 60)/longName(max 255) value exeeds max length!")
+        const returnedLocation = new Location(
+            await this.client.call(
+                "locations",
+                "POST",
+                {
+                    short: shortName,
+                    long: longName,
+                }
+            )
+        )
+        return returnedLocation
+    }
+
+    async edit(id: Number, shortName: string, longName: string | null): Promise<Location> {
+        if(longName == null) longName = shortName
+        if(shortName.length > 60 || longName.length > 255) throw new Error("Provided shortName(amx 60)/longName(max 255) value exeeds max length!")
+        const returnedLocation = new Location(
+            await this.client.call(
+                "locations/" + id,
+                "PATCH",
+                {
+                    short: shortName,
+                    long: longName,
+                }
+            )
+        )
+        return returnedLocation
+    }
+
+    async delete(id: Number) {
+        await this.client.call(
+            "locations/" + id,
+            "DELETE"
+        )
     }
 }

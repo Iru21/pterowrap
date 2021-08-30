6a89c0be1a0be51d2a7bc919548c15643197c97c
Iru21 <33987046+Iru21@users.noreply.github.com>
2021-08-30 19:00:06 +0200

    clean up, fix commit, github sucks, thank you

--- lib/managers/nodemanager.ts
+++ lib/managers/nodemanager.ts
@@ -104,4 +104,5 @@ export default class NodeManager {
     deleteAllocation(nodeId: string, allocationId: Number) {
         this.client.call("nodes/" + nodeId + "/allocations/" + allocationId , 'DELETE')
     }
+    
 }

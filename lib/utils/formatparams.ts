c251728495c592a757a8a67d3f88a0d690932c81
Iru21 <mateuszsamsel3@gmail.com>
2021-11-27 17:09:41 +0100

    add page param

--- lib/utils/formatparams.ts
+++ lib/utils/formatparams.ts
@@ -3,7 +3,7 @@ import { Types } from ".."
 export default function formatParams(parameters: Types.requestParameters): string {
     let result = ""
 
-    if (parameters.include || parameters.filters || parameters.sort || parameters.per_page || parameters.other) result += "?"
+    if (parameters.include || parameters.filters || parameters.sort || parameters.per_page || parameters.page || parameters.other) result += "?"
     if (parameters.include) {
         switch (typeof parameters.include) {
             case "string":
@@ -22,6 +22,7 @@ export default function formatParams(parameters: Types.requestParameters): strin
     }
     if (parameters.sort) result += `${addAnd(result)}sort=${parameters.sort.reverse ? "-" : ""}${parameters.sort.by}`
     if (parameters.per_page) result += `${addAnd(result)}per_page=${parameters.per_page}`
+    if (parameters.page) result += `${addAnd(result)}page=${parameters.page}`
     if (parameters.other) {
         for (let i = 0; i < parameters.other.length; i++) result += `${addAnd(result)}${parameters.other[i].key}=${parameters.other[i].value}`
     }
